48ae2c8159ff963d1dd68ebd7889e6023df9d477
sofoniasm <sofoniasmengistu@gmail.com>
2024-12-19 12:32:46 +0300

    add to my repo all i did

---
+++ src/scripts/subscriber_node.py
@@ -0,0 +1,16 @@
+from std_msgs.msg import String
+import rospy
+
+def callback(data):
+    rospy.loginfo("I heard: %s", data.data)
+
+def subscriber():
+    rospy.init_node('subscriber_node', anonymous=True)
+    rospy.Subscriber('talking_topic', String, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        subscriber()
+    except rospy.ROSInterruptException:
+        pass
